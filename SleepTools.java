80b2b7d19f03c6960108747f3186b3b94375d282
xxq <xuxq1015@163.com>
2019-04-12 13:35:34 +0800

    AQS--独占式获取锁

--- SleepTools.java
+++ SleepTools.java
@@ -3,35 +3,29 @@ package com.c503.hthj.asoco.dangerchemical.waste.util;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @auther xuxq
- * @date 2019/4/11 11:25
+ * 类说明：线程休眠辅助工具类
  */
 public class SleepTools {
-
-    /**
-     * 按照秒值休眠
-     * @param seconds
-     */
-    public static final void second(int seconds){
+	
+	/**
+	 * 按秒休眠
+	 * @param seconds 秒数
+	 */
+    public static final void second(int seconds) {
         try {
             TimeUnit.SECONDS.sleep(seconds);
         } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
-
+    
     /**
-     * 按照毫秒值休眠
-     * @param seconds
+     * 按毫秒数休眠
+     * @param seconds 毫秒数
      */
-    public static final void milliSecond(int seconds){
+    public static final void ms(int seconds) {
         try {
             TimeUnit.MILLISECONDS.sleep(seconds);
         } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
-
-
-
 }
