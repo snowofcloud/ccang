b7fbea21dde4862a388036a2d5b7cfa72a01a2c4
xxq <xuxq1015@163.com>
2019-04-27 11:15:11 +0800

    手动实现一个独占锁

---
+++ ch4/aqs/SelfLock.java
@@ -0,0 +1,87 @@
+package com.concurrent.ch4.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @auther xuxq
+ * @date 2019/4/27 10:52
+ */
+public class SelfLock implements Lock {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0,1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+        // 返回一个Condition，每个condition都包含了一个condition队列
+
+    }
+
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        System.out.println(Thread.currentThread().getName()+" ready get lock");
+        sync.acquire(1);
+        System.out.println(Thread.currentThread().getName()+" already got lock");
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public void unlock() {
+        System.out.println(Thread.currentThread().getName()+" ready release lock");
+        sync.release(1);
+        System.out.println(Thread.currentThread().getName()+" already released lock");
+    }
+
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+}
